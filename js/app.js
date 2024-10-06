af2130f1e0352724411ef0480d5e76512c0834ac
MdAfsarHossain <afsar291722@gmail.com>
2024-10-06 22:39:50 +0600

    Get all elements by id

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// All Elements
+const spinner = document.getElementById('spinner');
+const cardsContainer = document.getElementById('cardsContainer');
+const displayModal = document.getElementById('myModal');
+const rightSideContainer = document.getElementById('rightSideContainer');
+const categoriesContainer = document.getElementById('categoriesContainer');
+const noDataContainer = document.getElementById('noDataContainer');
+const petsDisplayContainer = document.getElementById('petsDisplayContainer');
+const handleSortByPrice = document.getElementById('handleSortByPrice');
+const adoptedModal = document.getElementById('adoptedModal');
+const countDownText = document.getElementById('countDownText');
